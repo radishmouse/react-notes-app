ca9133e66d288abdcdd88662bd1fdd32fd31d9e9
Chris Aquino <chris.aquino@gmail.com>
2018-07-24 14:45:28 -0400

    explains function components in comment in DocumentEditor.js

--- src/DocumentEditor.js
+++ src/DocumentEditor.js
@@ -10,4 +10,19 @@ class DocumentEditor extends React.Component {
   }
 }
 
+
+// A function component is like a
+// "render-only" component.
+// It can't have state.
+// It doesn't have lifecycle methods like
+// componentDidMount or constructor
+// const DocumentEditor = (props) => {
+//   return (
+//     <div className="document-editor">
+//       <textarea value="This is a note. See?" />
+//     </div>
+//   )
+// }
+
+
 export default DocumentEditor;
