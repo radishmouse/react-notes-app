9ac41e4676d364037690b06e4c7d21c980a638f8
Chris Aquino <chris.aquino@gmail.com>
2018-07-24 14:02:02 -0400

    adds document list items

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ class App extends React.Component {
   render() {
     return (
       <div className="notes-app">
+
         <header className="search-bar">
           <h1>React Notes App</h1>
           <input type="text" />
         </header>
 
-        <div className="document-list"></div>
+        <div className="document-list">
+          <ul>
+            <li className="document-list-item">Note numbah one</li>
+            <li className="document-list-item">Note numbah two</li>
+            <li className="document-list-item">Note numbah three</li>
+          </ul>
+        </div>
       </div>
     );
   }
